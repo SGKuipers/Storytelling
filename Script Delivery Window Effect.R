237ce3fd995cf06956653bd225cdbb714b5f9723
SGKuipers <78479028+SGKuipers@users.noreply.github.com>
2021-02-11 23:54:03 +0100

    Update Script Delivery Window Effect.R

--- Script Delivery Window Effect.R
+++ Script Delivery Window Effect.R
@@ -7,13 +7,14 @@ Deliverywindow<-factor(data_9$`Delivery window`,c("4 uur", "1 dag", "2 dagen", "
 
 
 ## Step 3 Plot the linegraph in the console.
-ggplot(data_9, aes(x=Deliverywindow, y= `Contract index`, colour=Suppliers, group=Suppliers))+
+ggplot(data_9, aes(x=Deliverywindow, y= `Contract index`, colour=Suppliers, group=Suppliers, fill=Suppliers))+
   geom_line(size=1.5)+
-  geom_line(aes(y=Contractindex1), color= "black", size=0.5, linetype = "dashed")+ ##additional dashed line
-  xlab("Delivery window in hours")+
+  geom_point(size=5, shape=21, colour="black", stroke=1.5)+
+  geom_line(aes(y=Contractindex1), color= "black", size=0.8, linetype = "dashed")+ ##additional dashed line
+  xlab("Delivery window")+
   ylab("Contract Index")+
-  ggtitle(label = "Delivery Window Effect", subtitle = "Effect Changes in Delivery Window on the Contractindex")+
-  theme(plot.title = element_text(color = "red", face = "bold", size = 32), ##themefucntion can be used to create specific thematic changes to the used data.
+  ggtitle(label = "Delivery Window Effect", subtitle = "Effect of Changes in Delivery window on the Contract Index")+
+  theme(plot.title = element_text(color = "red", face = "bold", size = 32), ##themefunction can be used to create specific thematic changes to the used data.
         plot.subtitle = element_text(color = "black", face = "italic", size = 18), ## subtitle
         axis.title.x = element_text(color = "black", size = 18), ##bigger x axis title
         axis.title.y = element_text(color = "black", size = 18)) ##bigger y axis title

0eb02ef63948597e78372eb3785870e7213f1944
SGKuipers <78479028+SGKuipers@users.noreply.github.com>
2021-03-11 13:33:34 +0100

    toevoegingen

---
+++ ggplot Gross Margin.R
@@ -0,0 +1,24 @@
+library(viridis) 
+library(hrbrthemes) 
+library(ggplot2) 
+library(dplyr) 
+
+
+
+Revenue2 <- aangepast_overzicht 
+Revenue2$z <- round((Revenue2$z*100),0)
+
+ggplot(Revenue2, aes(fill=group, label=paste0(Revenue2$z, "%"), y=y, x=x)) +  
+  
+  geom_bar(position="stack", stat="identity") + 
+  
+  scale_fill_viridis(discrete = T) + 
+  
+  ggtitle("Overview of Gross Margin for Customers") + 
+  
+  geom_text(size = 5, position = position_stack(vjust = 0.5))+ 
+  
+  theme_ipsum() + 
+  
+  xlab("Customers") +
+  ylab("Revenue in euro's")
